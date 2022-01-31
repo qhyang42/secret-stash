e92f30c36dd7a5b2698ea1c102306736373b9946
qhyang42 <57675612+qhyang42@users.noreply.github.com>
2022-01-31 12:12:11 -0600

    update jan 31

--- tvae_test/clustering.py
+++ tvae_test/clustering.py
@@ -1,11 +1,12 @@
 #%% cluster embedding with GMM
+import pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 
 # load data
-embed = np.load('C:/Users/qyx1327/Documents/results/tvae/results.npz')['embeddings']
+embed = np.load('C:/Users/qyx1327/Documents/results/tvae/result_msplit.npz')['embeddings']
 #split
 idx = np.random.choice(embed.shape[0], size=np.int32(np.floor(embed.shape[0]*0.2)), replace=False)
 embed_test = embed[idx, :]
@@ -13,6 +14,7 @@ mask = np.ones(embed.shape[0], dtype=bool)
 mask[idx] = False
 embed_train = embed[mask, :]
 
+#%%
 gm_bic = np.array([])
 
 for n_comp in range(140, 150):
@@ -20,23 +22,35 @@ for n_comp in range(140, 150):
     gm_bic = np.append(gm_bic, gm.bic(embed_test))
 
 
-#%% try aic
+#%% save the gmms
 
 # gm_aic = np.array([])
 gm = []
-
+gm_bic = np.array([])
 for n_comp in range(5, 100):
     gm.append(GaussianMixture(n_components=n_comp, covariance_type='diag', max_iter=1000).fit(embed_train))
-    # gm_aic = np.append(gm_aic, gm[-1].aic(embed_test))
+    gm_bic = np.append(gm_bic, gm[-1].bic(embed_test))
 
+with open('msplit_gmm_model.pickle', 'wb') as fhandle:
+    pickle.dump(gm, fhandle)
 
-
-#%% todo predict and save labels 
 #%% plot
-# xaxis = np.arange(5, 150)
-plt.plot(gm_aic)
-# plt.xlim([5, 150])
-# plt.xticks(np.arange(5, 150, 20))
+# xaxis = np.arange(5, 100)
+plt.plot(gm_bic)
+# plt.xlim([5, 100])
+# plt.xticks(np.arange(5, 100, 20))
 plt.xlabel('n_component')
-plt.ylabel('aic')
+plt.ylabel('bic')
 plt.show()
+
+#%%
+with open('gmm_model.pickle', 'rb') as fhandle:
+    gm = pickle.load(fhandle)
+
+gm_bic = np.array([])
+for m_idx in range(95):
+    gm_bic = np.append(gm_bic, gm[m_idx].bic(embed_test))
+
+#%% predict and save labels. run for one additional video and compare with annotation
+# choose n_component = 40
+current_model = gm[35]
--- tvae_test/prep_data.py
+++ tvae_test/prep_data.py
@@ -41,8 +41,77 @@ for i in range(out.shape[0]-slength):
     rs_out[i, :, :] = out[i:i+slength, :]
 
 # save
-np.savez('data_in.npz', data = rs_out)
+# np.savez('data_in.npz', data = rs_out)
 
+
+#%% plit data into training and testing
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+datadir = 'Y:/Parkerlab/Behavior/Clozapine'
+#datadir = '/Volumes/fsmresfiles/BasicSciences/Phys/Kennedylab/Parkerlab/Behavior/Clozapine'
+fileList_train = ['HighDose/Amph/output_v1_8/20191207_m085_clo04_amph/20191207_m085_clo04_amph_raw_feat_top_v1_8.npz',
+            'HighDose/Amph/output_v1_8/20191207_m971_clo04_amph/20191207_m971_clo04_amph_raw_feat_top_v1_8.npz',
+            'HighDose/Control/output_v1_8/20191207_m085_clo04/20191207_m085_clo04_raw_feat_top_v1_8.npz',
+            'HighDose/Control/output_v1_8/20191207_m971_clo04/20191207_m971_clo04_raw_feat_top_v1_8.npz',
+            'LowDose/Amph/output_v1_8/20191206_m971_clo05_amph/20191206_m971_clo05_amph_raw_feat_top_v1_8.npz',
+            'LowDose/Control/output_v1_8/20191206_m085_clo05/20191206_m085_clo05_raw_feat_top_v1_8.npz',
+            'Vehicle/Amph/output_v1_8/20191205_m085_clo01_amph/20191205_m085_clo01_amph_raw_feat_top_v1_8.npz',
+            'Vehicle/Amph/output_v1_8/20191205_m971_clo01_amph/20191205_m971_clo01_amph_raw_feat_top_v1_8.npz',
+            'Vehicle/Control/output_v1_8/20191205_m971_clo01/20191205_m971_clo01_raw_feat_top_v1_8.npz']
+            #read two file from each condition. same mice
+out = np.array([])
+for fileName in fileList_train:
+    data = np.load(os.path.join(datadir, fileName))
+    keypoints = data['keypoints']
+    keypoints = keypoints[:, 0, :]
+    x_kps = keypoints[:, 0]
+    y_kps = keypoints[:, 1]
+    out_this = np.zeros(keypoints.shape)
+    out_this = out_this.reshape(out_this.shape[0], -1)
+    out_this[:, ::2] = x_kps
+    out_this[:, 1::2] = y_kps
+    if out.size == 0:
+        out = out_this
+    else:
+        out = np.row_stack([out, out_this])
+
+# reshape ts
+slength = 10
+rs_out_train = np.zeros([out.shape[0], slength, out.shape[1]])
+for i in range(out.shape[0]-slength):
+    rs_out_train[i, :, :] = out[i:i+slength, :]
+
+
+fileList_test = [ 'LowDose/Amph/output_v1_8/20191206_m085_clo05_amph/20191206_m085_clo05_amph_raw_feat_top_v1_8.npz',
+                'LowDose/Control/output_v1_8/20191206_m971_clo05/20191206_m971_clo05_raw_feat_top_v1_8.npz',
+                'Vehicle/Control/output_v1_8/20191205_m085_clo01/20191205_m085_clo01_raw_feat_top_v1_8.npz',]
+                # read two file from each condition. same mice
+out = np.array([])
+for fileName in fileList_test:
+    data = np.load(os.path.join(datadir, fileName))
+    keypoints = data['keypoints']
+    keypoints = keypoints[:, 0, :]
+    x_kps = keypoints[:, 0]
+    y_kps = keypoints[:, 1]
+    out_this = np.zeros(keypoints.shape)
+    out_this = out_this.reshape(out_this.shape[0], -1)
+    out_this[:, ::2] = x_kps
+    out_this[:, 1::2] = y_kps
+    if out.size == 0:
+         out = out_this
+    else:
+        out = np.row_stack([out, out_this])
+
+# reshape ts
+slength = 10
+rs_out_test = np.zeros([out.shape[0], slength, out.shape[1]])
+for i in range(out.shape[0] - slength):
+     rs_out_test[i, :, :] = out[i:i + slength, :]
+
+# save
+np.savez('data_in_split.npz', data_train = rs_out_train, data_test = rs_out_test)
 
 #%% test
 # data1 = np.load(os.path.join(datadir, fileList[1]))
